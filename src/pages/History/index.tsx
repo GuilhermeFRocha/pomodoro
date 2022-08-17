c597b395484d88e75ece01dff90513e9ffe52668
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-08-17 20:30:44 -0300

    adicionando estilizaçao

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,67 @@
+import { HistoryContainer, HistoryList } from "./style"
+
+export const History = () => {
+  return (
+    <HistoryContainer>
+        <h1>History</h1>
+
+        <HistoryList>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duraçao</th>
+                <th>Inicio</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Tarefa 1</td>
+                <td>1:00</td>
+                <td>10/10/2020</td>
+                <td>Concluido</td>
+              </tr>
+
+              <tr>
+                <td>Tarefa 1</td>
+                <td>1:00</td>
+                <td>10/10/2020</td>
+                <td>Concluido</td>
+              </tr>
+
+              <tr>
+                <td>Tarefa 1</td>
+                <td>1:00</td>
+                <td>10/10/2020</td>
+                <td>Concluido</td>
+              </tr>
+
+              <tr>
+                <td>Tarefa 1</td>
+                <td>1:00</td>
+                <td>10/10/2020</td>
+                <td>Concluido</td>
+              </tr>
+
+              <tr>
+                <td>Tarefa 1</td>
+                <td>1:00</td>
+                <td>10/10/2020</td>
+                <td>Concluido</td>
+              </tr>
+
+              <tr>
+                <td>Tarefa 1</td>
+                <td>1:00</td>
+                <td>10/10/2020</td>
+                <td>Concluido</td>
+              </tr>
+            </tbody>
+          </table>
+        </HistoryList>
+    </HistoryContainer>
+  )
+}
+
