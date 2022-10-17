b12566d1d22e28358e83d44a958d2d7b2e8c3de8
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-10-17 10:39:39 -0300

    refatoring reducer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -37,7 +37,7 @@ export function Home() {
 
   const { handleSubmit, watch, reset } = newCycleForm;
 
-  function handleCreateNewCycle (data:NewCycleFormData) {
+  function handleCreateNewCycle (data: NewCycleFormData) {
     createNewCycle(data)
     reset()
   }
