87a69d130e0c4fad81003ecb76edf805f1ed6f06
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-10-17 14:45:44 -0300

    separating actions

--- src/contexts/CycleContexts.tsx
+++ src/contexts/CycleContexts.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState, useReducer } from "react";
-import {ActionTypes, Cycle, cyclesReducer} from '../redurces/cycles'
+import { ActionTypes, addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../redurces/cycles/actions";
+import {  Cycle, cyclesReducer} from '../redurces/cycles/reducer'
 interface CreateCycleData {
   task: string;
   minutesAmount: number;
@@ -44,12 +45,7 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(markCurrentCycleAsFinishedAction());
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -60,22 +56,12 @@ export function CyclesContextProvider({
       startDate: new Date(),
     };
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      },
-    });
+    dispatch(addNewCycleAction(newCycle));
     setAmountSecondsPassed(0);
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(interruptCurrentCycleAction());
   }
 
   return (
